cb78e27b3ed483e8bc040ddde1c2dc9543d2910c
Paweł Sydorów <pawel.sydorow@dev4you.pl>
2026-04-27 22:46:18 +0200

    Fixbuild

--- src/modules/portal/frontend/[orgSlug]/portal/dashboard/page.tsx
+++ src/modules/portal/frontend/[orgSlug]/portal/dashboard/page.tsx
@@ -4,6 +4,6 @@ type Props = {
   params: { orgSlug: string }
 }
 
-export default function PortalDashboardPage({ params }: Props) {
+export default function PortalDashboardPage({ params }: Props): never {
   redirect(`/${params.orgSlug}/portal/warranty-claims`)
 }
